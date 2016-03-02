d2905da442b052222069a3f6d046e889b696b273
ycossiny <ycossiny@gmail.com>
2016-03-02 16:32:28 +0900

    もろもろ
    マニフェストは直した。xmlも表示ができていない。また新しくactivity作った方が早いかもしれない。

--- HyouryouGanbare/app/src/main/java/com/example/yokoyama/hyouryouganbare/MainActivity.java
+++ HyouryouGanbare/app/src/main/java/com/example/yokoyama/hyouryouganbare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.yokoyama.hyouryouganbare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
     @Override
--- HyouryouGanbare/app/src/main/java/com/example/yokoyama/hyouryouganbare/ManyEleAct.java
+++ HyouryouGanbare/app/src/main/java/com/example/yokoyama/hyouryouganbare/ManyEleAct.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ManyEleAct extends AppCompatActivity {
 
@@ -22,8 +23,12 @@ public class ManyEleAct extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Button sendButton = (Button) findViewById(R.id.return_button);
+                sendButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                }
             }
-        });
-    }
-
-}
+        })
